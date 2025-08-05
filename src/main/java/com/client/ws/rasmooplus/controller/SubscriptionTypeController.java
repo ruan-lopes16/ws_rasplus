b15f56ece07f2e796143e212ce8e7d1031043cf6
ruan-lopes16 <sruan507@gmail.com>
2025-08-05 20:58:13 -0300

    service + impl and refactor SubscriptionTypeController

--- src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
+++ src/main/java/com/client/ws/rasmooplus/controller/SubscriptionTypeController.java
@@ -2,6 +2,7 @@ package com.client.ws.rasmooplus.controller;
 
 import com.client.ws.rasmooplus.model.SubscriptionType;
 import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,14 +20,14 @@ public class SubscriptionTypeController { // qualquer método aqui só será exe
     // Injeção de dependência. O Spring irá criar uma instância de SubscriptionTypeRepository
     // e a "injetar" nesta variável, tornando-a disponível para uso.
     @Autowired
-    private SubscriptionTypeRepository subscriptionTypeRepository; // BANCO
+    private SubscriptionTypeService subscriptionTypeService; // BANCO
 
     @GetMapping()   // dizendo que é uma requisição do tipo GET
     // O método retorna um 'ResponseEntity', permitindo um controle total da resposta HTTP.
     // O tipo de retorno é uma lista de objetos 'SubscriptionType'
     public ResponseEntity<List<SubscriptionType>> findAll(){
         // O método 'findAll' do repositório busca todos os registros na tabela 'subscriptions_type'.
-        List<SubscriptionType> subscriptionTypes = subscriptionTypeRepository.findAll();
+        List<SubscriptionType> subscriptionTypes = subscriptionTypeService.findAll();
 
         // Constrói e retorna a resposta HTTP.
         // ResponseEntity.status(HttpStatus.OK): Define o código de status HTTP como 200 (OK) >>> requisição bem-sucedida
---
+++ src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -0,0 +1,43 @@
+package com.client.ws.rasmooplus.service.impl;
+
+import com.client.ws.rasmooplus.model.SubscriptionType;
+import com.client.ws.rasmooplus.repository.SubscriptionTypeRepository;
+import com.client.ws.rasmooplus.service.SubscriptionTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
+
+    private final SubscriptionTypeRepository subscriptionTypeRepository; // importando Repository
+    // injeção ⬇️
+    public SubscriptionTypeServiceImpl(SubscriptionTypeRepository subscriptionTypeRepository) {
+        this.subscriptionTypeRepository = subscriptionTypeRepository;
+    }
+
+    @Override
+    public List<SubscriptionType> findAll() {
+        return subscriptionTypeRepository.findAll();
+    }
+
+    @Override
+    public SubscriptionType findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public SubscriptionType create(SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public SubscriptionType update(Long id, SubscriptionType subscriptionType) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
