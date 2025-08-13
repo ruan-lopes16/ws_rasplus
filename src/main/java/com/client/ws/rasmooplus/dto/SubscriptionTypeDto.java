b1e0d8e6b632b2212e69e4780bbd242e77a78ad6
ruan-lopes16 <sruan507@gmail.com>
2025-08-13 10:28:20 -0300

    refactor String on DTO > JavaxValidation

--- src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
+++ src/main/java/com/client/ws/rasmooplus/dto/SubscriptionTypeDto.java
@@ -18,17 +18,17 @@ import java.math.BigDecimal;
 public class SubscriptionTypeDto {
     private Long id;
 
-    @NotBlank(message = "field `name` cannot be null or empty") // não nulo e nem vazio
-    @Size(min = 5, max = 30, message = "the `name` field must be between 5 and 30 characters long") // tem que ter de 5 a 30 caracteres
+    @NotBlank(message = "cannot be null or empty") // não nulo e nem vazio
+    @Size(min = 5, max = 30, message = "must be between 5 and 30 characters long") // tem que ter de 5 a 30 caracteres
     private String name;
 
-    @Max(value = 12, message = "The `accessMonth` field cannot be greater than 12") // me permite colocar o máximo permitido, neste caso coloca-se value para colocar a quantidade + uma mensagem a ser exibida
+    @Max(value = 12, message = "cannot be greater than 12") // me permite colocar o máximo permitido, neste caso coloca-se value para colocar a quantidade + uma mensagem a ser exibida
     private Long accessMonths;
 
-    @NotNull(message = "field `price` cannot be null" )
+    @NotNull(message = "cannot be null" )
     private BigDecimal price;
 
-    @NotBlank(message = "field `productKey` cannot be null or empty")
-    @Size(min = 5, max = 15, message = "the `productKey` field must be between 5 and 15 characters long")
+    @NotBlank(message = "cannot be null or empty")
+    @Size(min = 5, max = 15, message = "must be between 5 and 15 characters long")
     private String productKey;
 }
