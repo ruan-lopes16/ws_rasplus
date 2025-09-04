509453b53fc604a8877d4313a204ca53e77754fb
ruan-lopes16 <sruan507@gmail.com>
2025-09-03 22:54:03 -0300

    claude help

--- src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
+++ src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
@@ -2,7 +2,7 @@ package com.client.ws.rasmooplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 // @EnableWebMvc // permite visualização do swagger
