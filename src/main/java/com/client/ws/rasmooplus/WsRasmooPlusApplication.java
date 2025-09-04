71e3ddd316d6161fee9481d941405a72faf0af48
ruan-lopes16 <sruan507@gmail.com>
2025-09-03 22:01:45 -0300

    test

--- src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
+++ src/main/java/com/client/ws/rasmooplus/WsRasmooPlusApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc // permite visualização do swagger
+// @EnableWebMvc // permite visualização do swagger
 public class WsRasmooPlusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WsRasmooPlusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WsRasmooPlusApplication.class, args);
+    }
 
 }
