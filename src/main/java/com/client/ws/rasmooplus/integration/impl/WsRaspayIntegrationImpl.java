d9c11b46a0b1221ac9b72c293f7996d7b0f4f179
ruan-lopes16 <sruan507@gmail.com>
2025-09-23 23:38:01 -0300

    implementing basic auth in API requests

--- src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
+++ src/main/java/com/client/ws/rasmooplus/integration/impl/WsRaspayIntegrationImpl.java
@@ -4,7 +4,9 @@ import com.client.ws.rasmooplus.dto.wsraspay.CustomerDto;
 import com.client.ws.rasmooplus.dto.wsraspay.OrderDto;
 import com.client.ws.rasmooplus.dto.wsraspay.PaymentDto;
 import com.client.ws.rasmooplus.integration.WsRaspayIntegration;
+import java.util.Base64;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -23,21 +25,24 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
 
     @Override
     public CustomerDto createCustomer(CustomerDto dto) {
-        // criação de http para enviar o body
-        HttpEntity<CustomerDto> request = new HttpEntity<>(dto); // não consegue passar como atributo, pois cada HttpEntity precisa de um determinado DTO
 
         try {
-            // response entity > retorno/resposta da chamada
+            HttpHeaders headers = getHttpHeaders();
 
+            // Criação da entidade HTTP contendo o DTO e os headers
+            HttpEntity<CustomerDto> request = new HttpEntity<>(dto, headers);
+
+            // response entity > retorno/resposta da chamada
+            // Chamada HTTP POST para o serviço externo
             ResponseEntity<CustomerDto> response =              // no momento esperamos que a resposta seja o proprio dto
                     restTemplate.exchange(
-                            "http://localhost:8081/ws-raspay/v1/costumer",
+                            "http://localhost:8081/ws-raspay/v1/customer",
                             HttpMethod.POST,
                             request,
                             CustomerDto.class
                     ); // url, verbo http, objeto de requisição, tipo de retorno
 
-            return response.getBody();
+            return response.getBody(); // Retorna o corpo da resposta
 
         } catch (Exception exception) {
             throw exception;
@@ -54,5 +59,21 @@ public class WsRaspayIntegrationImpl implements WsRaspayIntegration {
     @Override
     public Boolean processPayment(PaymentDto dto) {
         return null;
+    }
+
+    // isolando headers
+    private static HttpHeaders getHttpHeaders() {
+        // Criação dos headers HTTP
+        HttpHeaders headers = new HttpHeaders();
+
+        // Credenciais para autenticação básica
+        String credential = "rasmooplus:r@sm00";
+
+        // Codificação Base64 usando biblioteca padrão do Java
+        String base64 = Base64.getEncoder().encodeToString(credential.getBytes());
+
+        // Adicionando header de autorização Basic Auth
+        headers.add("Authorization", "Basic " + base64);
+        return headers;
     }
 }
